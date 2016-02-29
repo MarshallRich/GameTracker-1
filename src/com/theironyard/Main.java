c073c7d436ffc83b7763d202a9af98686bc68bd2
MarshallRich <rlmarsh@g.clemson.edu>
2016-02-29 14:44:00 -0500

    added database functionality

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,13 +5,18 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     static HashMap<String, User> users = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
         Spark.externalStaticFileLocation("public");
         Spark.init();
         Spark.get(
@@ -20,11 +25,13 @@ public class Main {
                     User user = getUserFromSession(request.session());
 
                     HashMap m = new HashMap<>();
+                    ArrayList<Game> games = selectGames(conn);
                     if (user == null) {
                         return new ModelAndView(m, "login.html");
                     }
                     else {
-                        return new ModelAndView(user, "home.html");
+                        m.put("games", games);
+                        return new ModelAndView(m, "home.html");
                     }
                 }),
                 new MustacheTemplateEngine()
@@ -66,14 +73,47 @@ public class Main {
                     if (gameName == null || gameGenre == null || gamePlatform == null) {
                         throw new Exception("Didn't receive all query parameters.");
                     }
-                    Game game = new Game(gameName, gameGenre, gamePlatform, gameYear);
+                    Game game = new Game(gameName, gameGenre, gamePlatform, gameYear, 0);
 
-                    user.games.add(game);
+                    insertGame(game, conn);
 
                     response.redirect("/");
                     return "";
                 })
         );
+        Spark.post(
+                "/delete-game",
+                ((request, response) -> {
+                int id = Integer.valueOf(request.queryParams("deleteNumber"));
+                    deleteGame(id, conn);
+
+                    response.redirect("/");
+                return "";
+                })
+        );
+
+        Spark.post(
+                "/edit-game",
+                ((request, response) -> {
+                int id = Integer.valueOf(request.queryParams("editNumber"));
+                    String editName = request.queryParams("editName");
+                    String editGenre = request.queryParams("editGenre");
+                    String editPlatform = request.queryParams("editPlatform");
+                    String editYearStr = request.queryParams("editYear");
+
+                    if (editYearStr != null || !editYearStr.isEmpty()){
+
+                    int editYear = Integer.valueOf(editYearStr);
+
+                        updateGame(id, editName, editGenre, editPlatform, editYear, conn);
+                    }
+
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
         Spark.post(
                 "/logout",
                 ((request, response) -> {
@@ -83,10 +123,67 @@ public class Main {
                     return "";
                 })
         );
+
+
     }
 
     static User getUserFromSession(Session session) {
         String name = session.attribute("userName");
         return users.get(name);
+    }
+
+    static void insertGame(Game game, Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+
+        stmt.execute("CREATE TABLE IF NOT EXISTS games (id IDENTITY, game_name VARCHAR, genre VARCHAR, platform VARCHAR, release_year INT)");
+
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO games VALUES (NULL, ?, ?, ?, ?)");
+
+
+        stmt2.setString(1, game.name);
+        stmt2.setString(2, game.genre);
+        stmt2.setString(3, game.platform);
+        stmt2.setInt(4, game.releaseYear);
+        stmt2.execute();
+    }
+
+    static void deleteGame(Integer id, Connection conn) throws SQLException {
+
+
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM games WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
+
+    }
+
+    static ArrayList<Game> selectGames(Connection conn) throws SQLException {
+        ArrayList<Game> games= new ArrayList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM games");
+        while(results.next()) {
+            String name = results.getString("game_name");
+            String genre = results.getString("genre");
+            String platform = results.getString("platform");
+            int releaseYear = results.getInt("release_year");
+            int id = results.getInt("id");
+            Game game = new Game(name, genre, platform, releaseYear, id);
+            games.add(game);
+        }
+        return games;
+    }
+
+    static void updateGame(int id, String name, String genre, String platform, int year, Connection conn) throws SQLException {
+
+        if ( !name.isEmpty() || !genre.isEmpty() || !platform.isEmpty() || name!=null || genre!=null || platform!=null) {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE games SET game_name = ?, genre = ?, platform = ?, release_year = ? WHERE id = ?");
+            stmt.setString(1, name);
+            stmt.setString(2, genre);
+            stmt.setString(3, platform);
+            stmt.setInt(4, year);
+            stmt.setInt(5, id);
+
+            stmt.execute();
+        }
+
     }
 }
